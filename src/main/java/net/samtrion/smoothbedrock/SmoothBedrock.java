c979d2890355671ce2a227bb22615fd1bd5ac5f1
samtrion <samtrion@gmail.com>
2018-05-24 11:32:31 +0200

    Port to Minecraft 1.9.4

--- src/main/java/net/samtrion/smoothbedrock/SmoothBedrock.java
+++ src/main/java/net/samtrion/smoothbedrock/SmoothBedrock.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.BlockPos;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
+import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 
 import net.minecraftforge.fml.common.IWorldGenerator;
@@ -20,26 +21,25 @@ public class SmoothBedrock implements IWorldGenerator {
 	public static final String MOD_ID = "smoothbedrock";
 	public static final String MOD_NAME = "SmoothBedrock";
 	public static final String MOD_VERSION = "@VERSION@";
-	public static final String MOD_DEPENDENCIES = "required-after:Forge@[11.15.1.1808,)";
+	public static final String MOD_DEPENDENCIES = "required-after:Forge@[12.17.0.1987,)";
 
 	@EventHandler
 	public void init(FMLInitializationEvent e) {
 		GameRegistry.registerWorldGenerator(this, Integer.MAX_VALUE);
 	}
-
+	
 	@Override
-	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkProvider generator,
-			IChunkProvider provider) {
+	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator generator, IChunkProvider provider) {
 		if (world.getWorldInfo().getTerrainType() == WorldType.FLAT) {
 			return;
 		}
 
-		int dimensionId = world.provider.getDimensionId();
+		int dimensionId = world.provider.getDimension();
 		if (dimensionId > 0 || dimensionId < -1) {
 			return;
 		}
 		boolean isNether = dimensionId == -1;
-		Block replaceBlock = isNether ? Blocks.netherrack : Blocks.stone;
+		Block replaceBlock = isNether ? Blocks.NETHERRACK : Blocks.STONE;
 
 		generateBedrock(world, chunkX, chunkZ, replaceBlock, 5, 4, 3, 2, 1);
 		if (isNether) {
@@ -56,7 +56,7 @@ public class SmoothBedrock implements IWorldGenerator {
 				for (int posY : yies) {
 					BlockPos pos = new BlockPos(posX, posY, posZ);
 					if (isBedrock(world, pos)) {
-						world.setBlockState(pos, replaceBlock.getStateFromMeta(0), 0);
+						world.setBlockState(pos, replaceBlock.getDefaultState(), 0);
 					}
 				}
 			}
@@ -64,6 +64,6 @@ public class SmoothBedrock implements IWorldGenerator {
 	}
 
 	private boolean isBedrock(World world, BlockPos pos) {
-		return world.getBlockState(pos).getBlock() == Blocks.bedrock;
+		return world.getBlockState(pos).getBlock() == Blocks.BEDROCK;
 	}
 }
